135cf4a1515b4ffa76c1a7fbc79fc24d518f9dd7
Lars Kaiser <emperor@kaiser.yt>
2018-06-07 19:14:16 +0200

    New minimal version of the clock with the new nested module system

---
+++ clock/module.lua
@@ -0,0 +1,76 @@
+local localized, CHILDS, CONTENTS = ...
+
+local M = {}
+
+local json = require "json"
+local res = "w"..NATIVE_WIDTH.."h"..NATIVE_HEIGHT
+local coords 
+
+local roboto = resource.load_font(localized "roboto.ttf")
+local robotob = resource.load_font(localized "robotob.ttf")
+local white = resource.create_colored_texture(1,1,1,1)
+local dot = resource.load_image("dot.png")
+local x, y, w, h
+local hours, hours12, minutes, seconds = 0
+local iso_date
+
+util.data_mapper{
+    ["hours"] = function(hour)
+        hours = tonumber(hour)
+        hours12 = hours % 12
+    end;
+    ["minutes"] = function(minute)
+        minutes = tonumber(minute)
+    end;
+    ["seconds"] = function(second)
+        seconds = tonumber(second)
+    end;
+    ["iso_date"] = function(iso)
+        iso_date = iso
+    end;
+}
+
+function zeiger(size, strength, winkel, r,g,b,a)
+    gl.pushMatrix()
+    gl.translate(w/2, h/2) 
+    gl.rotate(winkel, 0, 0, 1)
+    white:draw(0, -strength, size, strength)
+    gl.popMatrix()
+end
+
+function M.draw()
+    local fake_second = seconds * 1.05
+    if fake_second >= 60 then
+        fake_second = 60
+    end
+    
+    if hours, minutes, seconds then
+        zeiger(w/4,  10, 360/12 * hours12 - 90)
+        zeiger(w/2.5, 5, 360/60 * minutes - 90)
+        zeiger(w/2.1, 2, 360/60 * (((math.sin((fake_second-0.4) * math.pi*2)+1)/8) + fake_second) - 90)
+    end
+end
+
+function M.unload()
+    print "sub module is unloaded"
+end
+
+function M.content_update(name)
+    print("sub module content update", name)
+    if name == 'test.txt' then
+        text = resource.load_file(localized(name))
+    end
+    if name == 'coords.json' then
+        coords = json.decode(resource.load_file(localized(name)))
+        x = coords[res].x
+        y = coords[res].y
+        w = coords[res].w
+        h = coords[res].h
+    end
+end
+
+function M.content_remove(name)
+    print("sub module content delete", name)
+end
+
+return M
--- node.lua
+++ node.lua
@@ -1,51 +1,11 @@
 node.alias "*"
 util.noglobals()
 
-local settings = ({
-    ["w1280h1024"] = {
-        uhr = {
-            x = 0,
-            y = 0,
-            size = 200,
-        },
-        freifunk = {
-            x = 0,
-            y = 200,
-            size = 100,
-        },
-        termine = {
-            x = 0,
-            y = 300,
-            size = 100,
-        },
-    },
-    ["w1920h1080"] = {
-        test = {
-            x = 0,
-            y = 0,
-            w = 200,
-            h = 200,
-        },
-        freifunk = {
-            x = 0,
-            y = 200,
-            w = 200,
-            h = 200,
-        },
-        termine = {
-            x = 0,
-            y = 300,
-            w = 200,
-            h = 200,
-        },
-    },
-})["w"..NATIVE_WIDTH.."h"..NATIVE_HEIGHT]
-
 local loader = require "loader"
 
 gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
 function node.render()
     for name, module in pairs(loader.modules) do
-        module.draw("w"..NATIVE_WIDTH.."h"..NATIVE_HEIGHT)
+        module.draw()
     end
 end
